c85226da1b9b78a8e98667f573a4ecdeffa7f54f
mmandazi <amir.sahib@gmail.com>
2021-01-10 16:40:05 -0500

    year selector working now

--- src/components/series.js
+++ src/components/series.js
@@ -1,13 +1,33 @@
-import React from "react"
-import seriesData from "../content/series.json"
-
+import React from "react";
+import seriesData from "../content/series.json";
+import _ from 'lodash';
 export default class Series extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedYear: "2020",
+        };
+    }
+
+    setYear(event){
+        this.setState({selectedYear: event.target.value});
+    }
+
     render() {
+
+        const filteredData = _.filter(seriesData.content,{"year":this.state.selectedYear});
+        console.log('filteredData',filteredData);
         return (
             <div className="ramadan-series-selector">
+                <h3>Select Year</h3>
+                <select value={this.state.selectedYear} onChange={(event)=>this.setYear(event)}>
+                    <option value="2020">2020/1441</option>
+                    <option value="2019">2019/1440</option>
+                </select>
+
                 <h3>Select Ramadan Series:</h3>
-                {seriesData.content.map((data,index) =>  {
+                {filteredData.map((data,index) =>  {
                     return  <a href={`series/`+data.url} key={`ramadan-series-${index}`}>
                                 <span className="ramadan-series-name">{data.name}</span>
                                 <span className="ramadan-series-speaker">{data.speakers.join(', ')}</span>
