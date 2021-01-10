d8b87fce5e727d6d8b91fe746c20f42a89fc2d46
mmandazi <amir.sahib@gmail.com>
2021-01-10 16:23:27 -0500

    clean up yq 2020 series json, remove test md files, add os 2019 series

--- src/components/episode.js
+++ src/components/episode.js
@@ -3,12 +3,13 @@ import React from "react"
 export default class Episode extends React.Component {
     
     render() {
+        console.log(this.props.episode);
         return (
             <div className="series-episode-wrapper">
                 <h3>{this.props.episode.name}</h3>
                 <h4>Episode: {this.props.episode.episode}</h4>
                 <p>{this.props.episode.description}</p>
-                <iframe title={this.props.episode.name} width="560" height="315" src={`https://www.youtube.com/embed/${this.props.episode.youtube_id}`} frameborder="0" allowfullscreen></iframe>
+                <iframe title={this.props.episode.name} width="560" height="315" src={`https://www.youtube.com/embed/${this.props.episode.youtube_id}`} frameBorder="0" allowFullScreen></iframe>
             </div>
         )
     }
--- src/pages/os-ep-1.md
+++
@@ -1,8 +0,0 @@
----
-title: "Episode 1: They've Got Your Back | Angels in Your Presence | Ramadan Series"
-date: "2020-04-24"
----
-
-Do we have such a thing as guardian angels? What do they do, and how do we experience them? 
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/9VtsVxVyn1g" frameborder="0" allowfullscreen></iframe>
